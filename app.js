be20d0dfa58bca20042d1112e60c34a1810d9823
Nick <nkrause@gmail.com>
2021-09-18 18:05:38 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+//If below is left enabled it breaks the server
+//const request = require("request");
+
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName,
+      }
+    }]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us5.api.mailchimp.com/3.0/lists/d845c9bd67";
+
+  const options = {
+    method: "POST",
+    auth: "nick1:2e4c276d0e4839591a2f2a1d772132c7-us5"
+  };
+
+  const request = https.request(url, options, function(response) {
+
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    });
+  });
+//  request.write(jsonData);
+//  request.end();
+
+});
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running on port 3000");
+});
+
+
+//API  Mailchimp
+// 2e4c276d0e4839591a2f2a1d772132c7-us5
+
+//Audicence ID
+// d845c9bd67
